4ff15a91658505a2fec38e64cbed207544f84558
Antoine Delbos <aadelbos@gmail.com>
2022-02-03 16:31:32 +0100

    add uggly style

--- script.js
+++ script.js
@@ -11,28 +11,55 @@ function getUrlParam() {
 function removeButtonCreation(parent, parent2) { 
     const removeButton = document.createElement('button')
     removeButton.classList.add('btn')
-    removeButton.innerText = 'supprimer l\'action'
     parent.appendChild(removeButton)
+    if (parent2) {
+        removeButton.innerText = 'supprimer l\'action'
+        removeButton.classList.add('removeAction')
+    } else {
+        removeButton.innerText = 'supprimer la liste'
+        removeButton.classList.add('removeList')
+    }
     removeButton.addEventListener('click',() => {
         if (parent2) {
             parent2.remove()
+            removeButton.innerText = 'supprimer l\'action'
         } else {
             parent.remove()
+            removeButton.innerText = 'supprimer la liste'
         }
         removeButton.remove()
     })
+    return removeButton
 }
 
 function listFillling(form, input, actionContainer, div) {
     form.addEventListener('submit', (e )=>{
     e.preventDefault()
+    if(input.value !== null && input.value !== ''){
+    const pContainer = document.createElement('div')   
+    pContainer.classList.add('action')
+    actionContainer.appendChild(pContainer)
     const p = document.createElement('p')
     p.innerText = `${input.value}`
     input.value = ''
+    input.placeholder = 'votre prochaine tache à réaliser';
     actionContainer.appendChild(p)
-    removeButtonCreation(actionContainer, p)
+    pContainer.appendChild(p)
+    removeButtonCreation(actionContainer, pContainer)
+    } else {
+        input.placeholder = 'veuillez rentrer une action'
+        input.style.border = '1px solid red'
+        input.style.color = 'red'
+        input.addEventListener('focus', () => {
+            input.style.border = '1px solid black'
+            input.style.color = 'black' 
+        })
+        input.addEventListener('keypress', () => {
+            input.style.border = '1px solid black'
+            input.style.color = 'black' 
+        })
+    }
     })
-    console.log(div);
 }
 
 function createForm(listName, div) {
@@ -44,16 +71,19 @@ function createForm(listName, div) {
     const actionContainer = document.createElement('div')
     actionContainer.classList.add('actionContainer')
     let label = document.createElement('label');
+    label.classList.add('labelList')
     label.for = `${listName}`;
     label.innerText = listName + ' : \n'
     let input = document.createElement('input');
     input.type = 'TEXT';
     input.name = `${listName}`
-    input.value = 'votre tache à réaliser';
+    input.placeholder = 'votre tache à réaliser';
+    input.classList.add('input')
     let button = document.createElement('input');
     button.type = 'submit'
-    button.value = 'créer'
-    button.class = 'btn submit'
+    button.value = 'créer une action'
+    button.classList.add('submitedBtn')
+    button.classList.add('btn')
     // attache des éléments les un aux autres
     // label, input, button => div action
     actionContainer.appendChild(label)
